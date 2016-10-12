0b14eb0664db28f5160495d2855ec3037b1a0e90
Siebren88 <siebrenkraak@hotmail.com>
2016-10-12 10:58:29 +0200

    MenuItems handler
    Created class that generates design items like the DateTimePickers /
    labels.

---
+++ Barroc-IT/Barroc-IT/MenuItems.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Barroc_IT
+{
+    class MenuItems
+    {
+
+        public ToolStripControlHost[] DTPGenerator() 
+        {
+            DateTimePicker dtp_From = new DateTimePicker() { Width = 140 };
+            DateTimePicker dtp_Till = new DateTimePicker() { Width = 140 };
+            Label lbl_From = new Label() { Text = "From:", Width = 140 };
+            Label lbl_Till = new Label() { Text = "Till:", Width = 140 };
+
+            ToolStripControlHost lblch_From = new ToolStripControlHost(lbl_From);
+            ToolStripControlHost lblch_Till = new ToolStripControlHost(lbl_Till);
+            ToolStripControlHost tsch_From = new ToolStripControlHost(dtp_From);
+            ToolStripControlHost tsch_Till = new ToolStripControlHost(dtp_Till);
+
+            ToolStripControlHost[] arrayControl = new ToolStripControlHost[4];
+            arrayControl[0] = new ToolStripControlHost(lbl_From);
+            arrayControl[1] = new ToolStripControlHost(dtp_From);
+            arrayControl[2] = new ToolStripControlHost(lbl_Till);
+            arrayControl[3] = new ToolStripControlHost(dtp_Till);
+
+            return arrayControl;
+        }
+    }
+}
