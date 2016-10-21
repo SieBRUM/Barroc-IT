d6e02c738bb638a41f9a996d34cc752e3eac853a
m-vd-linden <m-vd-linden@hotmail.com>
2016-10-21 11:07:12 +0200

    Sales
    Edited user control: CustomerPanel.cs

--- Barroc-IT/Barroc-IT/CustomerPanel.Designer.cs
+++ Barroc-IT/Barroc-IT/CustomerPanel.Designer.cs
@@ -29,26 +29,14 @@
         private void InitializeComponent()
         {
             this.panel2 = new System.Windows.Forms.Panel();
+            this.lbl_Customer_CompanyName = new System.Windows.Forms.Label();
+            this.lbl_CompanyName = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
             this.lbl_Unpaid_Invoice = new System.Windows.Forms.Label();
             this.lbl_Customer_ID = new System.Windows.Forms.Label();
             this.lbl_Customer_Name = new System.Windows.Forms.Label();
-            this.lbl_Deadline = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.lbl_Amount_Invoices = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.lbl_Hardware = new System.Windows.Forms.Label();
-            this.lbl_Maintenance_Contract = new System.Windows.Forms.Label();
-            this.lbl_Operating_System = new System.Windows.Forms.Label();
-            this.lbl_Internal_Contact_Person = new System.Windows.Forms.Label();
-            this.lbl_Company_Name = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label7 = new System.Windows.Forms.Label();
-            this.label8 = new System.Windows.Forms.Label();
-            this.label9 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
+            this.lbl_Customer_Address_1 = new System.Windows.Forms.Label();
+            this.lbl_Address_1 = new System.Windows.Forms.Label();
             this.panel2.SuspendLayout();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
@@ -56,27 +44,34 @@
             // panel2
             // 
             this.panel2.BackColor = System.Drawing.SystemColors.MenuHighlight;
-            this.panel2.Controls.Add(this.lbl_Deadline);
-            this.panel2.Controls.Add(this.label4);
-            this.panel2.Controls.Add(this.lbl_Amount_Invoices);
-            this.panel2.Controls.Add(this.label3);
-            this.panel2.Controls.Add(this.lbl_Hardware);
-            this.panel2.Controls.Add(this.lbl_Maintenance_Contract);
-            this.panel2.Controls.Add(this.lbl_Operating_System);
-            this.panel2.Controls.Add(this.lbl_Internal_Contact_Person);
-            this.panel2.Controls.Add(this.lbl_Company_Name);
-            this.panel2.Controls.Add(this.label1);
-            this.panel2.Controls.Add(this.label7);
-            this.panel2.Controls.Add(this.label8);
-            this.panel2.Controls.Add(this.label9);
-            this.panel2.Controls.Add(this.label2);
-            this.panel2.Controls.Add(this.label6);
-            this.panel2.Controls.Add(this.label5);
+            this.panel2.Controls.Add(this.lbl_Customer_Address_1);
+            this.panel2.Controls.Add(this.lbl_Address_1);
+            this.panel2.Controls.Add(this.lbl_Customer_CompanyName);
+            this.panel2.Controls.Add(this.lbl_CompanyName);
             this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel2.Location = new System.Drawing.Point(0, 46);
             this.panel2.Name = "panel2";
             this.panel2.Size = new System.Drawing.Size(538, 364);
             this.panel2.TabIndex = 3;
+            // 
+            // lbl_Customer_CompanyName
+            // 
+            this.lbl_Customer_CompanyName.AutoSize = true;
+            this.lbl_Customer_CompanyName.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Customer_CompanyName.Location = new System.Drawing.Point(128, 19);
+            this.lbl_Customer_CompanyName.Name = "lbl_Customer_CompanyName";
+            this.lbl_Customer_CompanyName.Size = new System.Drawing.Size(74, 13);
+            this.lbl_Customer_CompanyName.TabIndex = 36;
+            this.lbl_Customer_CompanyName.Text = "Placeholder";
+            // 
+            // lbl_CompanyName
+            // 
+            this.lbl_CompanyName.AutoSize = true;
+            this.lbl_CompanyName.Location = new System.Drawing.Point(12, 19);
+            this.lbl_CompanyName.Name = "lbl_CompanyName";
+            this.lbl_CompanyName.Size = new System.Drawing.Size(83, 13);
+            this.lbl_CompanyName.TabIndex = 35;
+            this.lbl_CompanyName.Text = "Company name:";
             // 
             // panel1
             // 
@@ -120,157 +115,24 @@
             this.lbl_Customer_Name.TabIndex = 0;
             this.lbl_Customer_Name.Text = "Customer Name";
             // 
-            // lbl_Deadline
+            // lbl_Customer_Address_1
             // 
-            this.lbl_Deadline.AutoSize = true;
-            this.lbl_Deadline.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbl_Deadline.Location = new System.Drawing.Point(318, 86);
-            this.lbl_Deadline.Name = "lbl_Deadline";
-            this.lbl_Deadline.Size = new System.Drawing.Size(74, 13);
-            this.lbl_Deadline.TabIndex = 36;
-            this.lbl_Deadline.Text = "Placeholder";
+            this.lbl_Customer_Address_1.AutoSize = true;
+            this.lbl_Customer_Address_1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Customer_Address_1.Location = new System.Drawing.Point(128, 36);
+            this.lbl_Customer_Address_1.Name = "lbl_Customer_Address_1";
+            this.lbl_Customer_Address_1.Size = new System.Drawing.Size(74, 13);
+            this.lbl_Customer_Address_1.TabIndex = 38;
+            this.lbl_Customer_Address_1.Text = "Placeholder";
             // 
-            // label4
+            // lbl_Address_1
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(146, 86);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(76, 13);
-            this.label4.TabIndex = 35;
-            this.label4.Text = "Deadline date:";
-            // 
-            // lbl_Amount_Invoices
-            // 
-            this.lbl_Amount_Invoices.AutoSize = true;
-            this.lbl_Amount_Invoices.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbl_Amount_Invoices.Location = new System.Drawing.Point(318, 223);
-            this.lbl_Amount_Invoices.Name = "lbl_Amount_Invoices";
-            this.lbl_Amount_Invoices.Size = new System.Drawing.Size(74, 13);
-            this.lbl_Amount_Invoices.TabIndex = 34;
-            this.lbl_Amount_Invoices.Text = "Placeholder";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(146, 223);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(100, 13);
-            this.label3.TabIndex = 33;
-            this.label3.Text = "Amount of invoices:";
-            // 
-            // lbl_Hardware
-            // 
-            this.lbl_Hardware.AutoSize = true;
-            this.lbl_Hardware.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbl_Hardware.Location = new System.Drawing.Point(318, 266);
-            this.lbl_Hardware.Name = "lbl_Hardware";
-            this.lbl_Hardware.Size = new System.Drawing.Size(74, 13);
-            this.lbl_Hardware.TabIndex = 32;
-            this.lbl_Hardware.Text = "Placeholder";
-            // 
-            // lbl_Maintenance_Contract
-            // 
-            this.lbl_Maintenance_Contract.AutoSize = true;
-            this.lbl_Maintenance_Contract.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbl_Maintenance_Contract.Location = new System.Drawing.Point(318, 185);
-            this.lbl_Maintenance_Contract.Name = "lbl_Maintenance_Contract";
-            this.lbl_Maintenance_Contract.Size = new System.Drawing.Size(74, 13);
-            this.lbl_Maintenance_Contract.TabIndex = 31;
-            this.lbl_Maintenance_Contract.Text = "Placeholder";
-            // 
-            // lbl_Operating_System
-            // 
-            this.lbl_Operating_System.AutoSize = true;
-            this.lbl_Operating_System.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbl_Operating_System.Location = new System.Drawing.Point(318, 245);
-            this.lbl_Operating_System.Name = "lbl_Operating_System";
-            this.lbl_Operating_System.Size = new System.Drawing.Size(74, 13);
-            this.lbl_Operating_System.TabIndex = 30;
-            this.lbl_Operating_System.Text = "Placeholder";
-            // 
-            // lbl_Internal_Contact_Person
-            // 
-            this.lbl_Internal_Contact_Person.AutoSize = true;
-            this.lbl_Internal_Contact_Person.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbl_Internal_Contact_Person.Location = new System.Drawing.Point(318, 162);
-            this.lbl_Internal_Contact_Person.Name = "lbl_Internal_Contact_Person";
-            this.lbl_Internal_Contact_Person.Size = new System.Drawing.Size(74, 13);
-            this.lbl_Internal_Contact_Person.TabIndex = 29;
-            this.lbl_Internal_Contact_Person.Text = "Placeholder";
-            // 
-            // lbl_Company_Name
-            // 
-            this.lbl_Company_Name.AutoSize = true;
-            this.lbl_Company_Name.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbl_Company_Name.Location = new System.Drawing.Point(318, 139);
-            this.lbl_Company_Name.Name = "lbl_Company_Name";
-            this.lbl_Company_Name.Size = new System.Drawing.Size(74, 13);
-            this.lbl_Company_Name.TabIndex = 28;
-            this.lbl_Company_Name.Text = "Placeholder";
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(318, 117);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(74, 13);
-            this.label1.TabIndex = 27;
-            this.label1.Text = "Placeholder";
-            // 
-            // label7
-            // 
-            this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(146, 185);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(114, 13);
-            this.label7.TabIndex = 26;
-            this.label7.Text = "Maintenance contract:";
-            // 
-            // label8
-            // 
-            this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(146, 266);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(56, 13);
-            this.label8.TabIndex = 25;
-            this.label8.Text = "Hardware:";
-            // 
-            // label9
-            // 
-            this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(146, 245);
-            this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(93, 13);
-            this.label9.TabIndex = 24;
-            this.label9.Text = "Operating System:";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(146, 162);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(119, 13);
-            this.label2.TabIndex = 23;
-            this.label2.Text = "Internal contact person:";
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(146, 117);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(83, 13);
-            this.label6.TabIndex = 21;
-            this.label6.Text = "Customer name:";
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(146, 139);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(83, 13);
-            this.label5.TabIndex = 22;
-            this.label5.Text = "Company name:";
+            this.lbl_Address_1.AutoSize = true;
+            this.lbl_Address_1.Location = new System.Drawing.Point(12, 36);
+            this.lbl_Address_1.Name = "lbl_Address_1";
+            this.lbl_Address_1.Size = new System.Drawing.Size(57, 13);
+            this.lbl_Address_1.TabIndex = 37;
+            this.lbl_Address_1.Text = "Address 1:";
             // 
             // CustomerPanel
             // 
@@ -296,21 +158,9 @@
         private System.Windows.Forms.Label lbl_Unpaid_Invoice;
         private System.Windows.Forms.Label lbl_Customer_ID;
         private System.Windows.Forms.Label lbl_Customer_Name;
-        private System.Windows.Forms.Label lbl_Deadline;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label lbl_Amount_Invoices;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label lbl_Hardware;
-        private System.Windows.Forms.Label lbl_Maintenance_Contract;
-        public System.Windows.Forms.Label lbl_Operating_System;
-        private System.Windows.Forms.Label lbl_Internal_Contact_Person;
-        private System.Windows.Forms.Label lbl_Company_Name;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.Label label8;
-        private System.Windows.Forms.Label label9;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lbl_Customer_CompanyName;
+        private System.Windows.Forms.Label lbl_CompanyName;
+        private System.Windows.Forms.Label lbl_Customer_Address_1;
+        private System.Windows.Forms.Label lbl_Address_1;
     }
 }
