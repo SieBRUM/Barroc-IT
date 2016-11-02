4c897846d3ed94013602a4c7b5cca46897b8b358
m-vd-linden <m-vd-linden@hotmail.com>
2016-11-02 11:38:52 +0100

    Finance_2
    Added invoicePanel

---
+++ Barroc-IT/Barroc-IT/InvoicePanel.Designer.cs
@@ -0,0 +1,333 @@
+﻿namespace Barroc_IT
+{
+    partial class InvoicePanel
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lbl_Unpaid_Invoice = new System.Windows.Forms.Label();
+            this.lbl_Customer_Name = new System.Windows.Forms.Label();
+            this.lbl_Invoice_ID = new System.Windows.Forms.Label();
+            this.lbl_Price = new System.Windows.Forms.Label();
+            this.lbl_Customer_CompanyName = new System.Windows.Forms.Label();
+            this.lbl_CompanyName = new System.Windows.Forms.Label();
+            this.lbl_Customer_Contact_Person = new System.Windows.Forms.Label();
+            this.lbl_Internal_Contact_Person = new System.Windows.Forms.Label();
+            this.lbl_Invoice_Tax_Percentage = new System.Windows.Forms.Label();
+            this.lbl_Tax_Percentage = new System.Windows.Forms.Label();
+            this.lbl_Project_ProjectID = new System.Windows.Forms.Label();
+            this.lbl_ProjectID = new System.Windows.Forms.Label();
+            this.lbl_Project_AmountofInvoices = new System.Windows.Forms.Label();
+            this.lbl_AmountofInvoices = new System.Windows.Forms.Label();
+            this.lbl_Customer_IBAN = new System.Windows.Forms.Label();
+            this.lbl_IBAN = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.lbl_Customer_Discount = new System.Windows.Forms.Label();
+            this.lbl_Discount = new System.Windows.Forms.Label();
+            this.panel2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.panel2.Controls.Add(this.lbl_Customer_Discount);
+            this.panel2.Controls.Add(this.lbl_Discount);
+            this.panel2.Controls.Add(this.label9);
+            this.panel2.Controls.Add(this.label10);
+            this.panel2.Controls.Add(this.lbl_Customer_IBAN);
+            this.panel2.Controls.Add(this.lbl_IBAN);
+            this.panel2.Controls.Add(this.lbl_Project_AmountofInvoices);
+            this.panel2.Controls.Add(this.lbl_AmountofInvoices);
+            this.panel2.Controls.Add(this.lbl_Project_ProjectID);
+            this.panel2.Controls.Add(this.lbl_ProjectID);
+            this.panel2.Controls.Add(this.lbl_Invoice_Tax_Percentage);
+            this.panel2.Controls.Add(this.lbl_Tax_Percentage);
+            this.panel2.Controls.Add(this.lbl_Customer_Contact_Person);
+            this.panel2.Controls.Add(this.lbl_Internal_Contact_Person);
+            this.panel2.Controls.Add(this.lbl_Customer_CompanyName);
+            this.panel2.Controls.Add(this.lbl_CompanyName);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 46);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(538, 119);
+            this.panel2.TabIndex = 5;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.panel1.Controls.Add(this.lbl_Price);
+            this.panel1.Controls.Add(this.lbl_Invoice_ID);
+            this.panel1.Controls.Add(this.lbl_Unpaid_Invoice);
+            this.panel1.Controls.Add(this.lbl_Customer_Name);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(538, 46);
+            this.panel1.TabIndex = 4;
+            // 
+            // lbl_Unpaid_Invoice
+            // 
+            this.lbl_Unpaid_Invoice.AutoSize = true;
+            this.lbl_Unpaid_Invoice.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Unpaid_Invoice.Location = new System.Drawing.Point(326, 17);
+            this.lbl_Unpaid_Invoice.Name = "lbl_Unpaid_Invoice";
+            this.lbl_Unpaid_Invoice.Size = new System.Drawing.Size(92, 13);
+            this.lbl_Unpaid_Invoice.TabIndex = 2;
+            this.lbl_Unpaid_Invoice.Text = "Unpaid invoice";
+            // 
+            // lbl_Customer_Name
+            // 
+            this.lbl_Customer_Name.AutoSize = true;
+            this.lbl_Customer_Name.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Customer_Name.Location = new System.Drawing.Point(12, 17);
+            this.lbl_Customer_Name.Name = "lbl_Customer_Name";
+            this.lbl_Customer_Name.Size = new System.Drawing.Size(95, 13);
+            this.lbl_Customer_Name.TabIndex = 0;
+            this.lbl_Customer_Name.Text = "Customer Name";
+            // 
+            // lbl_Invoice_ID
+            // 
+            this.lbl_Invoice_ID.AutoSize = true;
+            this.lbl_Invoice_ID.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Invoice_ID.Location = new System.Drawing.Point(190, 17);
+            this.lbl_Invoice_ID.Name = "lbl_Invoice_ID";
+            this.lbl_Invoice_ID.Size = new System.Drawing.Size(66, 13);
+            this.lbl_Invoice_ID.TabIndex = 3;
+            this.lbl_Invoice_ID.Text = "Invoice ID";
+            // 
+            // lbl_Price
+            // 
+            this.lbl_Price.AutoSize = true;
+            this.lbl_Price.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Price.Location = new System.Drawing.Point(480, 17);
+            this.lbl_Price.Name = "lbl_Price";
+            this.lbl_Price.Size = new System.Drawing.Size(36, 13);
+            this.lbl_Price.TabIndex = 4;
+            this.lbl_Price.Text = "Price";
+            // 
+            // lbl_Customer_CompanyName
+            // 
+            this.lbl_Customer_CompanyName.AutoSize = true;
+            this.lbl_Customer_CompanyName.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Customer_CompanyName.Location = new System.Drawing.Point(146, 19);
+            this.lbl_Customer_CompanyName.Name = "lbl_Customer_CompanyName";
+            this.lbl_Customer_CompanyName.Size = new System.Drawing.Size(74, 13);
+            this.lbl_Customer_CompanyName.TabIndex = 38;
+            this.lbl_Customer_CompanyName.Text = "Placeholder";
+            // 
+            // lbl_CompanyName
+            // 
+            this.lbl_CompanyName.AutoSize = true;
+            this.lbl_CompanyName.Location = new System.Drawing.Point(12, 19);
+            this.lbl_CompanyName.Name = "lbl_CompanyName";
+            this.lbl_CompanyName.Size = new System.Drawing.Size(83, 13);
+            this.lbl_CompanyName.TabIndex = 37;
+            this.lbl_CompanyName.Text = "Company name:";
+            // 
+            // lbl_Customer_Contact_Person
+            // 
+            this.lbl_Customer_Contact_Person.AutoSize = true;
+            this.lbl_Customer_Contact_Person.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Customer_Contact_Person.Location = new System.Drawing.Point(146, 36);
+            this.lbl_Customer_Contact_Person.Name = "lbl_Customer_Contact_Person";
+            this.lbl_Customer_Contact_Person.Size = new System.Drawing.Size(74, 13);
+            this.lbl_Customer_Contact_Person.TabIndex = 40;
+            this.lbl_Customer_Contact_Person.Text = "Placeholder";
+            // 
+            // lbl_Internal_Contact_Person
+            // 
+            this.lbl_Internal_Contact_Person.AutoSize = true;
+            this.lbl_Internal_Contact_Person.Location = new System.Drawing.Point(12, 36);
+            this.lbl_Internal_Contact_Person.Name = "lbl_Internal_Contact_Person";
+            this.lbl_Internal_Contact_Person.Size = new System.Drawing.Size(119, 13);
+            this.lbl_Internal_Contact_Person.TabIndex = 39;
+            this.lbl_Internal_Contact_Person.Text = "Internal contact person:";
+            // 
+            // lbl_Invoice_Tax_Percentage
+            // 
+            this.lbl_Invoice_Tax_Percentage.AutoSize = true;
+            this.lbl_Invoice_Tax_Percentage.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Invoice_Tax_Percentage.Location = new System.Drawing.Point(442, 36);
+            this.lbl_Invoice_Tax_Percentage.Name = "lbl_Invoice_Tax_Percentage";
+            this.lbl_Invoice_Tax_Percentage.Size = new System.Drawing.Size(74, 13);
+            this.lbl_Invoice_Tax_Percentage.TabIndex = 42;
+            this.lbl_Invoice_Tax_Percentage.Text = "Placeholder";
+            // 
+            // lbl_Tax_Percentage
+            // 
+            this.lbl_Tax_Percentage.AutoSize = true;
+            this.lbl_Tax_Percentage.Location = new System.Drawing.Point(326, 36);
+            this.lbl_Tax_Percentage.Name = "lbl_Tax_Percentage";
+            this.lbl_Tax_Percentage.Size = new System.Drawing.Size(85, 13);
+            this.lbl_Tax_Percentage.TabIndex = 41;
+            this.lbl_Tax_Percentage.Text = "Tax percentage:";
+            // 
+            // lbl_Project_ProjectID
+            // 
+            this.lbl_Project_ProjectID.AutoSize = true;
+            this.lbl_Project_ProjectID.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Project_ProjectID.Location = new System.Drawing.Point(442, 19);
+            this.lbl_Project_ProjectID.Name = "lbl_Project_ProjectID";
+            this.lbl_Project_ProjectID.Size = new System.Drawing.Size(74, 13);
+            this.lbl_Project_ProjectID.TabIndex = 44;
+            this.lbl_Project_ProjectID.Text = "Placeholder";
+            // 
+            // lbl_ProjectID
+            // 
+            this.lbl_ProjectID.AutoSize = true;
+            this.lbl_ProjectID.Location = new System.Drawing.Point(326, 19);
+            this.lbl_ProjectID.Name = "lbl_ProjectID";
+            this.lbl_ProjectID.Size = new System.Drawing.Size(57, 13);
+            this.lbl_ProjectID.TabIndex = 43;
+            this.lbl_ProjectID.Text = "Project ID:";
+            // 
+            // lbl_Project_AmountofInvoices
+            // 
+            this.lbl_Project_AmountofInvoices.AutoSize = true;
+            this.lbl_Project_AmountofInvoices.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Project_AmountofInvoices.Location = new System.Drawing.Point(146, 87);
+            this.lbl_Project_AmountofInvoices.Name = "lbl_Project_AmountofInvoices";
+            this.lbl_Project_AmountofInvoices.Size = new System.Drawing.Size(74, 13);
+            this.lbl_Project_AmountofInvoices.TabIndex = 46;
+            this.lbl_Project_AmountofInvoices.Text = "Placeholder";
+            // 
+            // lbl_AmountofInvoices
+            // 
+            this.lbl_AmountofInvoices.AutoSize = true;
+            this.lbl_AmountofInvoices.Location = new System.Drawing.Point(12, 87);
+            this.lbl_AmountofInvoices.Name = "lbl_AmountofInvoices";
+            this.lbl_AmountofInvoices.Size = new System.Drawing.Size(97, 13);
+            this.lbl_AmountofInvoices.TabIndex = 45;
+            this.lbl_AmountofInvoices.Text = "Amount of invoices";
+            // 
+            // lbl_Customer_IBAN
+            // 
+            this.lbl_Customer_IBAN.AutoSize = true;
+            this.lbl_Customer_IBAN.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Customer_IBAN.Location = new System.Drawing.Point(146, 70);
+            this.lbl_Customer_IBAN.Name = "lbl_Customer_IBAN";
+            this.lbl_Customer_IBAN.Size = new System.Drawing.Size(74, 13);
+            this.lbl_Customer_IBAN.TabIndex = 48;
+            this.lbl_Customer_IBAN.Text = "Placeholder";
+            // 
+            // lbl_IBAN
+            // 
+            this.lbl_IBAN.AutoSize = true;
+            this.lbl_IBAN.Location = new System.Drawing.Point(12, 70);
+            this.lbl_IBAN.Name = "lbl_IBAN";
+            this.lbl_IBAN.Size = new System.Drawing.Size(35, 13);
+            this.lbl_IBAN.TabIndex = 47;
+            this.lbl_IBAN.Text = "IBAN:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(146, 53);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(74, 13);
+            this.label9.TabIndex = 50;
+            this.label9.Text = "Placeholder";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(12, 53);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(123, 13);
+            this.label10.TabIndex = 49;
+            this.label10.Text = "Ledger account number:";
+            // 
+            // lbl_Customer_Discount
+            // 
+            this.lbl_Customer_Discount.AutoSize = true;
+            this.lbl_Customer_Discount.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Customer_Discount.Location = new System.Drawing.Point(442, 53);
+            this.lbl_Customer_Discount.Name = "lbl_Customer_Discount";
+            this.lbl_Customer_Discount.Size = new System.Drawing.Size(74, 13);
+            this.lbl_Customer_Discount.TabIndex = 52;
+            this.lbl_Customer_Discount.Text = "Placeholder";
+            // 
+            // lbl_Discount
+            // 
+            this.lbl_Discount.AutoSize = true;
+            this.lbl_Discount.Location = new System.Drawing.Point(326, 53);
+            this.lbl_Discount.Name = "lbl_Discount";
+            this.lbl_Discount.Size = new System.Drawing.Size(109, 13);
+            this.lbl_Discount.TabIndex = 51;
+            this.lbl_Discount.Text = "Discount percentage:";
+            // 
+            // InvoicePanel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.MinimumSize = new System.Drawing.Size(538, 0);
+            this.Name = "InvoicePanel";
+            this.Size = new System.Drawing.Size(538, 165);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lbl_Unpaid_Invoice;
+        private System.Windows.Forms.Label lbl_Customer_Name;
+        private System.Windows.Forms.Label lbl_Price;
+        private System.Windows.Forms.Label lbl_Invoice_ID;
+        private System.Windows.Forms.Label lbl_Customer_CompanyName;
+        private System.Windows.Forms.Label lbl_CompanyName;
+        private System.Windows.Forms.Label lbl_Customer_Discount;
+        private System.Windows.Forms.Label lbl_Discount;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label lbl_Customer_IBAN;
+        private System.Windows.Forms.Label lbl_IBAN;
+        private System.Windows.Forms.Label lbl_Project_AmountofInvoices;
+        private System.Windows.Forms.Label lbl_AmountofInvoices;
+        private System.Windows.Forms.Label lbl_Project_ProjectID;
+        private System.Windows.Forms.Label lbl_ProjectID;
+        private System.Windows.Forms.Label lbl_Invoice_Tax_Percentage;
+        private System.Windows.Forms.Label lbl_Tax_Percentage;
+        private System.Windows.Forms.Label lbl_Customer_Contact_Person;
+        private System.Windows.Forms.Label lbl_Internal_Contact_Person;
+
+
+    }
+}
