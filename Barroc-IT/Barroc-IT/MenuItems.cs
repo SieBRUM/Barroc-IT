243af41ab4c492426be43ba77138701ef5a590ad
m-vd-linden <m-vd-linden@hotmail.com>
2016-11-08 12:22:12 +0100

    Menuitem.cs
    Added constructor for development

--- Barroc-IT/Barroc-IT/MenuItems.cs
+++ Barroc-IT/Barroc-IT/MenuItems.cs
@@ -38,5 +38,35 @@ namespace Barroc_IT
 
             return arrayControl;
         }
+
+        public static ToolStripControlHost[] DTPGenerator(frm_Development form)
+        {
+            DateTimePicker dtp_From = new DateTimePicker() { Width = 140 };
+            //dtp_From.ValueChanged += new System.EventHandler(form.SetDateFromLabel);
+            DateTimePicker dtp_Till = new DateTimePicker() { Width = 140 };
+            //dtp_Till.ValueChanged += new System.EventHandler(form.SetDateTillLabel);
+            Label lbl_From = new Label() { Text = "From:", Width = 140 };
+            Label lbl_Till = new Label() { Text = "Till:", Width = 140 };
+            Button btn = new Button() { Text = "Search" };
+            //btn.Click += new System.EventHandler(form.SearchNotificationOnDate);
+            dtp_From.Value = DateTime.Now.Date;
+            dtp_Till.Value = DateTime.Now.Date;
+
+            ToolStripControlHost lblch_From = new ToolStripControlHost(lbl_From);
+            ToolStripControlHost lblch_Till = new ToolStripControlHost(lbl_Till);
+            ToolStripControlHost tsch_From = new ToolStripControlHost(dtp_From);
+            ToolStripControlHost tsch_Till = new ToolStripControlHost(dtp_Till);
+            ToolStripControlHost tsch_btn = new ToolStripControlHost(btn);
+
+
+            ToolStripControlHost[] arrayControl = new ToolStripControlHost[5];
+            arrayControl[0] = new ToolStripControlHost(lbl_From);
+            arrayControl[1] = new ToolStripControlHost(dtp_From);
+            arrayControl[2] = new ToolStripControlHost(lbl_Till);
+            arrayControl[3] = new ToolStripControlHost(dtp_Till);
+            arrayControl[4] = new ToolStripControlHost(btn);
+
+            return arrayControl;
+        }
     }
 }
